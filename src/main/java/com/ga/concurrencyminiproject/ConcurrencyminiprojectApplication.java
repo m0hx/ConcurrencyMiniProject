8d3fe9226156635a334518d8404335e5cff5358f
M0HX <M0HX@users.noreply.github.com>
2026-03-31 15:13:24 +0300

    test.

--- src/main/java/com/ga/concurrencyminiproject/ConcurrencyminiprojectApplication.java
+++ src/main/java/com/ga/concurrencyminiproject/ConcurrencyminiprojectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConcurrencyminiprojectApplication {
 
+	// test
 	public static void main(String[] args) {
 		SpringApplication.run(ConcurrencyminiprojectApplication.class, args);
 	}
