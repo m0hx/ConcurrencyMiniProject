4875a10e0be7a740a9359cede61749ac74471ae6
M0HX <M0HX@users.noreply.github.com>
2026-03-31 13:57:49 +0300

    Initial commit

---
+++ src/main/java/com/ga/concurrencyminiproject/ConcurrencyminiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ga.concurrencyminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyminiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyminiprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ga/concurrencyminiproject/ConcurrencyminiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.concurrencyminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyminiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
