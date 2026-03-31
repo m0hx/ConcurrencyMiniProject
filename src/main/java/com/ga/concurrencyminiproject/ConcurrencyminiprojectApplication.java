9cf55ba955a476076ab716c832cc41356688101b
M0HX <M0HX@users.noreply.github.com>
2026-03-31 15:19:00 +0300

    Renamed branch to main.

--- src/main/java/com/ga/concurrencyminiproject/ConcurrencyminiprojectApplication.java
+++ src/main/java/com/ga/concurrencyminiproject/ConcurrencyminiprojectApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConcurrencyminiprojectApplication {
 
-	// test
 	public static void main(String[] args) {
 		SpringApplication.run(ConcurrencyminiprojectApplication.class, args);
 	}
